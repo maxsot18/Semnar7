3c631fc51e15357f383a798f3497d4346266ee6f
Maxsot <sotnikov.maxim3007@gmail.com>
2022-12-18 07:16:47 -0800

    TAsk01

---
+++ task01/Program.cs
@@ -0,0 +1,44 @@
+﻿// Задача 1: Задайте двумерный массив размером m×n, заполненный случайными целыми числами.
+// m = 3, n = 4.
+// 1     4    8   19
+// 5   -2  33   -2
+// 77  3    8     1
+
+int[,] Array(int[,] array)
+{
+
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = new Random().Next(1, 10);
+        }
+    }
+    return array;
+}
+
+int Prompt(string message)
+{
+    Console.Write($"{message} ");
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+void PrintMatrix(int[,] matr)
+{
+    for (int i = 0; i < matr.GetLength(0); i++)
+    {
+        for (int j = 0; j < matr.GetLength(1); j++)
+        {
+            Console.Write($"{matr[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+    
+
+}
+
+int m = Prompt("Enter a quantity of strings ");
+int n = Prompt("Enter a quantity of pillars ");
+int[,] arr = new int[m, n];
+PrintMatrix(Array(arr));
+
