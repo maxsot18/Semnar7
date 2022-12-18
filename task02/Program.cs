1aa492f9a8db4f29e4b0c620ac88971a553faa20
Maxsot <sotnikov.maxim3007@gmail.com>
2022-12-18 07:31:13 -0800

    task02

---
+++ task02/Program.cs
@@ -0,0 +1,44 @@
+﻿// Задача 1: Задайте двумерный массив размера m на n, 
+// каждый элемент в массиве находится по формуле: 
+// Aₘₙ = m+n. Выведите полученный массив на экран.
+// m = 3, n = 4.
+// 0 1 2 3
+// 1 2 3 4
+// 2 3 4 5
+
+void PrintMatrix(int[,] matr)
+{
+    for (int i = 0; i < matr.GetLength(0); i++)
+    {
+        for (int j = 0; j < matr.GetLength(1); j++)
+        {
+            Console.Write($"{matr[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+    
+
+}
+int Prompt(string message)
+{
+    Console.Write($"{message} ");
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+int[,] Array(int[,] array, int m, int n)
+{
+
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = i + j;
+        }
+    }
+    return array;
+}
+
+int m = Prompt("Enter a quantity of strings ");
+int n = Prompt("Enter a quantity of pillars ");
+int[,] arr = new int[m, n];
+PrintMatrix(Array(arr, m, n));
