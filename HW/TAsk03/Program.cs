213ecd847a2bf422c73d204c24ce696f55d582f4
Maxsot <sotnikov.maxim3007@gmail.com>
2022-12-22 07:49:54 -0800

    Task03

---
+++ HW/TAsk03/Program.cs
@@ -0,0 +1,50 @@
+﻿// Задача 3. Задайте двумерный массив из целых чисел. 
+// Найдите среднее арифметическое элементов в каждом столбце.
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// Среднее арифметическое каждого столбца: 4,6; 5,6; 3,6; 3.
+
+void PrintMatrix(int[,] matr)
+{
+    for (int i = 0; i < matr.GetLength(0); i++)
+    {
+        for (int j = 0; j < matr.GetLength(1); j++)
+        {
+
+            matr[i, j] = new Random().Next(1, 10);
+            Console.Write($"{matr[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+
+
+}
+void AverageOfEachColumns(int[,] matr)
+{
+
+
+    for (int i = 0; i < matr.GetLength(0); i++)
+    {
+        double sum = 0;
+        double average = 0;
+
+
+        for (int j = 0; j < matr.GetLength(1); j++)
+        {
+
+            sum = matr[i, j] + sum;
+
+            average = sum / matr.GetLength(1);
+
+
+
+        }
+        Console.WriteLine($"Average is of {i + 1} column {average}");
+    }
+}
+
+int[,] matr = new int[5, 5];
+PrintMatrix(matr);
+AverageOfEachColumns(matr);
